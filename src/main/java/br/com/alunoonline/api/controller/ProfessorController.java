3b3f8642063b20444233133cd4beeccdac370536
Caio Dias <caiodevgit@gmail.com>
2026-04-06 16:59:54 -0300

    todo o CRUD de professor

--- src/main/java/br/com/alunoonline/api/controller/ProfessorController.java
+++ src/main/java/br/com/alunoonline/api/controller/ProfessorController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/Professores")
 public class ProfessorController
@@ -18,5 +21,36 @@ public class ProfessorController
     public void criarAluno(@RequestBody Professor professor)
     {
         professorService.criarProfessor(professor);
+    }
+
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Professor> listarTodosProfessores()
+    {
+        return professorService.listarTodosProfessores();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Optional<Professor> buscarProfessorPorId(@PathVariable Long id)
+    {
+        return professorService.buscarProfessorPorId(id);
+    }
+
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarProfessorPorId(@PathVariable Long id)
+    {
+        professorService.deletarProfessorPorId(id);
+    }
+
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizarProfessorPorId(@PathVariable Long id, @RequestBody Professor professorEditado) //body do JSON vai ser o professorEditado
+    {
+        professorService.atualizarProfessorPorId(id, professorEditado);
     }
 }
--- src/main/java/br/com/alunoonline/api/service/ProfessorService.java
+++ src/main/java/br/com/alunoonline/api/service/ProfessorService.java
@@ -5,6 +5,9 @@ import br.com.alunoonline.api.repository.ProfessorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ProfessorService
 {
@@ -14,5 +17,28 @@ public class ProfessorService
     public void criarProfessor(Professor professor)
     {
         professorRepository.save(professor);
+    }
+
+
+    public List<Professor> listarTodosProfessores()
+    {
+        return professorRepository.findAll();
+    }
+    public Optional<Professor> buscarProfessorPorId(Long id)
+    {
+        return professorRepository.findById(id);
+    }
+
+
+    public void deletarProfessorPorId(Long id)
+    {
+        professorRepository.deleteById(id);
+    }
+
+
+    public void atualizarProfessorPorId(Long id, Professor professorEditado)
+    {
+        professorEditado.setId(id);
+        professorRepository.save(professorEditado);
     }
 }
